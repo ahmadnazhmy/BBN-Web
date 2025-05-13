e96674b12c78f4547864418ecea5693dbffd2efc
ahmadnazhmy <ahmadnazhmy@gmail.com>
2025-05-13 17:46:45 +0700

    be3 commit

--- be/src/app.js
+++ be/src/app.js
@@ -25,6 +25,16 @@ app.use(cors({
 
 app.options('*', cors()); 
 
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res.status(200).end();  
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
