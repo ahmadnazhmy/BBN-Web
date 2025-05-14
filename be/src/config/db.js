6d804cae2fefca7bc560c46609eb8e8e737e6d0c
ahmadnazhmy <ahmadnazhmy@gmail.com>
2025-05-14 10:57:20 +0700

    db connect

--- be/src/config/db.js
+++ be/src/config/db.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+const dbUrl = process.env.DATABASE_URL;
+
+const url = new URL(dbUrl);
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'bbn_web',
-  port: 3306
+  host: url.hostname,             
+  user: url.username,               
+  password: url.password || '',    
+  database: url.pathname.substring(1),
+  port: url.port || 3306,           
 });
 
 module.exports = pool;
