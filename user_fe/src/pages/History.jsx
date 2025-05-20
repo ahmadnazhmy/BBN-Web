0135621cb637fd394cdaee69c0a595c30a4c8f9b
ahmadnazhmy <ahmadnazhmy@gmail.com>
2025-05-20 07:07:43 +0700

    jwt

--- user_fe/src/pages/History.jsx
+++ user_fe/src/pages/History.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
