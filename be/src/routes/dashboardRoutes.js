9faddcef5530c0b4191e7250fb7e7501b57eb4f9
ahmadnazhmy <ahmadnazhmy@gmail.com>
2025-06-01 14:40:31 +0700

    autenticate di dashboard

--- be/src/routes/dashboardRoutes.js
+++ be/src/routes/dashboardRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
+const { authenticate } = require('../middlewares/authMiddleware');
 
-router.get('/summary', dashboardController.getDashboardSummary);
+router.get('/summary', authenticate, dashboardController.getDashboardSummary);
 
 module.exports = router;
