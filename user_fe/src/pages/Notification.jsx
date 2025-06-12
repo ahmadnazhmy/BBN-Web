5f6faf4165a24f66c23bd44972498a15a47c7ee2
ahmadnazhmy <ahmadnazhmy@gmail.com>
2025-06-12 22:04:32 +0700

    Notif

--- user_fe/src/pages/Notification.jsx
+++ user_fe/src/pages/Notification.jsx
@@ -179,7 +179,7 @@ const Notification = ({ isOpen, setIsOpen, notificationButtonRef, setParentNotif
                 className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
                 aria-label="Tutup notifikasi"
               >
-                <FontAwesomeIcon icon={faTimes} className="text-xl" />
+                <FontAwesomeIcon icon={faXmark} className="text-xl" />
               </button>
             </div>
           </div>
